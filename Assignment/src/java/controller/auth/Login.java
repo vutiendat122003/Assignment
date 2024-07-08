82d54e2112aa2bc305d956a3f84b6dcced60067f
dat300412 <133365602+dat300412@users.noreply.github.com>
2024-07-08 22:31:12 +0700

    update login
    thông báo lỗi ngay ở trang và cho đăng nhập lại

--- Assignment/src/java/controller/auth/Login.java
+++ Assignment/src/java/controller/auth/Login.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller.auth;
 
 import dal.UserDBContext;
@@ -19,19 +18,20 @@ import model.User;
  * @author Vanxuan
  */
 public class Login extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-   
-
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -39,12 +39,13 @@ public class Login extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         request.getRequestDispatcher("view/auth/login.jsp").forward(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -52,37 +53,36 @@ public class Login extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        
+
         UserDBContext db = new UserDBContext();
         User user = db.getUserByUsernamePassword(username, password);
-        if(user !=null)
-        {
+        if (user != null) {
             request.getSession().setAttribute("user", user);
-        
-        // Kiểm tra vai trò của người dùng
-        String role = user.getRole();
-        
-        if ("teacher".equalsIgnoreCase(role)) {
-             response.sendRedirect("exam/lecturer"); // Chuyển hướng đến trang giảng viên
-        } else if ("student".equalsIgnoreCase(role)) {
-            response.sendRedirect("semester/student"); // Chuyển hướng đến trang sinh viên
+
+            // Kiểm tra vai trò của người dùng
+            String role = user.getRole();
+
+            if ("teacher".equalsIgnoreCase(role)) {
+                response.sendRedirect("exam/lecturer"); // Chuyển hướng đến trang giảng viên
+            } else if ("student".equalsIgnoreCase(role)) {
+                response.sendRedirect("semester/student"); // Chuyển hướng đến trang sinh viên
+            } else {
+                response.getWriter().println("Role not recognized!");
+            }
         } else {
-            response.getWriter().println("Role not recognized!");
-        }
-        }
-        else
-        {
-            response.getWriter().println("login failed!");
+            // Thêm thông báo lỗi vào request
+            request.setAttribute("errorMessage", "Incorrect username or password");
+            // Chuyển hướng trở lại trang đăng nhập
+            request.getRequestDispatcher("view/auth/login.jsp").forward(request, response);
         }
     }
-       
-    
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
--- Assignment/web/view/auth/login.jsp
+++ Assignment/web/view/auth/login.jsp
@@ -9,25 +9,27 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-         <link href="${pageContext.request.contextPath}/file/text/basic.css" rel="stylesheet" type="text/css"/>
-         <link href="${pageContext.request.contextPath}/file/text/logout.css" rel="stylesheet" type="text/css"/>
+        <link href="${pageContext.request.contextPath}/file/text/basic.css" rel="stylesheet" type="text/css"/>
+        <link href="${pageContext.request.contextPath}/file/text/logout.css" rel="stylesheet" type="text/css"/>
         <title>JSP Page</title>
     </head>
     <body>
         <form action="login" method="POST" class="login-form">
-           <label for="username">Username:</label>
-           <input type="text" id="username" name="username"/><br/>
-           <label for="password">Password:</label>
-           <input type="password" id="password" name="password"/> <br/>           
-           <input type="submit" value="Login"/>
-           <label>
-               <input type="checkbox" name="remember" value="remember"/> Remember for 1 week!
-           </label>
-           <div class="redText">
-               ${requestScope.error}
-           </div>
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username"/><br/>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password"/> <br/> 
+            <% if(request.getAttribute("errorMessage") != null) { %>
+            <div style="color: red; margin-top: 10px;">
+                <%= request.getAttribute("errorMessage") %>
+            </div>
+            <% } %>
+            <br/> 
+            <input type="submit" value="Login"/>           
+
         </form>
-           <a href="${pageContext.request.contextPath}/logout" class="logout-btn">Logout</a>
+
+        <a href="${pageContext.request.contextPath}/logout" class="logout-btn">Logout</a>
 
     </body>
 </html>
